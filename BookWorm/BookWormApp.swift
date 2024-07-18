44f003551429ad6c764e77888122f13d9138dc2a
aditya-0212 <adityachoubisa02@gmail.com>
2024-07-18 06:52:14 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by APPLE on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
